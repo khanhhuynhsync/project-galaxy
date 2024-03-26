20b2c2f355226623bf5bce15799eb32b3373600c
Khanh Huynh <khanh.huynh@knistr.com>
2024-03-26 15:44:18 +0700

    refactor: refactor project

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,11 @@
 import '../assets/stylesheet/body.css';
 
 function Body() {
-  return (
-    <div className='container body-background my-2'>
-      <h1>Body</h1>
-    </div>
-  );
+    return (
+        <div className='container body-background my-2'>
+            <h1>Body</h1>
+        </div>
+    );
 }
 
 export default Body;
